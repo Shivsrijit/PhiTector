607e7531127c073ae165a32278568ead5d28e641
try3d <rsarans186@gmail.com>
2024-04-18 12:16:57 +0530

    Bro shit sucks

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
 from fastapi import FastAPI
 from typing import Optional
 
+import numpy as np
 import pickle
+import sqlite3
 
 from utils import *
+
+conn = sqlite3.connect("cache.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS cache
+                  (url TEXT PRIMARY KEY, 
+                   lin_pred INTEGER, 
+                   log_pred INTEGER, 
+                   knn_pred INTEGER,
+                   usingIP INTEGER,
+                   longURL INTEGER,
+                   shortURL INTEGER,
+                   havingAtSymbol INTEGER,
+                   doubleSlashRedirect INTEGER,
+                   havingDashSymbol INTEGER,
+                   havingSubDomain INTEGER,
+                   havingSSLCert INTEGER,
+                   domainRegLen INTEGER,
+                   havingFavicon INTEGER,
+                   port INTEGER,
+                   httpsToken INTEGER,
+                   externalObjects INTEGER,
+                   anchorTags INTEGER,
+                   metadata INTEGER,
+                   suspiciousSFH INTEGER,
+                   emailSubmission INTEGER,
+                   legitimateWebsite INTEGER,
+                   redirectLegitimacy INTEGER,
+                   mouseOver INTEGER,
+                   rightClick INTEGER,
+                   popupWindow INTEGER,
+                   iframe INTEGER,
+                   domainLegitimacy INTEGER,
+                   dnsRecord INTEGER,
+                   ranking INTEGER,
+                   pageRank INTEGER,
+                   googleIndex INTEGER)"""
+)
+conn.commit()
 
 app = FastAPI()
 
@@ -21,6 +63,14 @@ with open("knn.pkl", "rb") as f:
 def index(url: Optional[str] = None) -> dict:
     if not url:
         return {"error": "Please provide a url"}
+
+    cursor.execute("SELECT * FROM cache WHERE url=?", (url,))
+    cached_result = cursor.fetchone()
+
+    if cached_result:
+        column_names = [description[0] for description in cursor.description]
+        cached_data = dict(zip(column_names, cached_result))
+        return cached_data
 
     using_ip = is_using_ip(url)
     long_url = is_long_url(url)
@@ -41,9 +91,101 @@ def index(url: Optional[str] = None) -> dict:
     email_submission = submit_to_email(url)
     legitimate_website = check_legitimate_website(url)
     redirect_legitimacy = check_redirects_legitimacy(url)
+    mouse_over = check_fake_url_status_bar(url)
+    check_right_click = check_disable_right_click(url)
+    check_popup_window = popup_window(url)
+    check_iframes = check_invisible_iframes(url)
+    domain_legitimacy = check_domain_legitimacy(url)
+    dns_record = check_dns_and_whois(url)
+    check_ranking = check_alexa_rank(url)
+    page_ranking = get_page_rank(url)
+    google_index = check_google_index(url)
+
+    arr = [
+        using_ip,
+        long_url,
+        short_url,
+        at_symbol,
+        slash_redirect,
+        dash_symbol,
+        sub_domain,
+        ssl_cert,
+        reg_len,
+        favicon,
+        sockets,
+        https_token,
+        external_objects,
+        check_tags,
+        metadata,
+        susp_sfh,
+        email_submission,
+        legitimate_website,
+        redirect_legitimacy,
+        mouse_over,
+        check_right_click,
+        check_popup_window,
+        check_iframes,
+        domain_legitimacy,
+        dns_record,
+        check_ranking,
+        page_ranking,
+        google_index,
+    ]
+
+    x_val = np.array(arr).reshape(1, -1)
+
+    lin_pred = lin_pic.predict(x_val)[0]
+    log_pred = log_pic.predict(x_val)[0]
+    knn_pred = knn_pic.predict(x_val)[0]
+
+    print(lin_pred)
+    print(log_pred)
+    print(knn_pred)
+
+    cursor.execute(
+        "INSERT OR REPLACE INTO cache VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (
+            url,
+            lin_pred,
+            log_pred,
+            knn_pred,
+            using_ip,
+            long_url,
+            short_url,
+            at_symbol,
+            slash_redirect,
+            dash_symbol,
+            sub_domain,
+            ssl_cert,
+            reg_len,
+            favicon,
+            sockets,
+            https_token,
+            external_objects,
+            check_tags,
+            metadata,
+            susp_sfh,
+            email_submission,
+            legitimate_website,
+            redirect_legitimacy,
+            mouse_over,
+            check_right_click,
+            check_popup_window,
+            check_iframes,
+            domain_legitimacy,
+            dns_record,
+            check_ranking,
+            page_ranking,
+            google_index,
+        ),
+    )
+
+    conn.commit()
 
     return {
-        "accuracy": 100,
+        "lin_pred": lin_pred,
+        "log_pred": log_pred,
+        "knn_pred": knn_pred,
         "usingIP": using_ip,
         "longURL": long_url,
         "shortURL": short_url,
@@ -63,6 +205,15 @@ def index(url: Optional[str] = None) -> dict:
         "emailSubmission": email_submission,
         "legitimateWebsite": legitimate_website,
         "redirectLegitimacy": redirect_legitimacy,
+        "mouseOver": mouse_over,
+        "rightClick": check_right_click,
+        "popupWindow": check_popup_window,
+        "iframe": check_iframes,
+        "domainLegitimacy": domain_legitimacy,
+        "dnsRecord": dns_record,
+        "ranking": check_ranking,
+        "pageRank": page_ranking,
+        "googleIndex": google_index,
     }
 
 
--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import ssl
+import re
+from datetime import datetime
+import alexapy
+from googlesearch import search
 
 
 def is_using_ip(url: str) -> int:
@@ -78,7 +82,7 @@ def get_domain_reg_len(url: str) -> int:
         if registration_length / 365 <= 1:
             return 1
         return 0
-    except Exception:
+    except:
         return 1
 
 
@@ -93,7 +97,7 @@ def check_favicon(url: str) -> int:
             return 1
         else:
             return 0
-    except Exception:
+    except:
         return 0
 
 
@@ -104,7 +108,7 @@ def check_all_ports_open(url: str) -> int:
             s.settimeout(1)
             s.connect((url, port))
             s.close()
-        except Exception:
+        except:
             return 0
     return 1
 
@@ -115,7 +119,7 @@ def check_https_token(url: str) -> int:
         with socket.create_connection((url, 443)) as sock:
             with context.wrap_socket(sock, server_hostname=url):
                 return 1
-    except Exception:
+    except:
         return 0
 
 
@@ -132,7 +136,7 @@ def check_external_objects(url: str) -> int:
             if object_domain != main_domain:
                 return 1
         return 0
-    except Exception:
+    except:
         return 1
 
 
@@ -153,7 +157,7 @@ def check_anchor_tags(url: str) -> int:
             if href_domain and href_domain != main_domain:
                 return 1
         return 0
-    except Exception:
+    except:
         return 0
 
 
@@ -172,7 +176,7 @@ def check_metadata_tags(url: str) -> int:
                 return 1
 
         return 0
-    except Exception:
+    except:
         return 0
 
 
@@ -189,7 +193,9 @@ def check_suspicious_sfh(url: str) -> int:
 
 
 def submit_to_email(url: str) -> int:
-    return url.lower().endswith(".php") or url.lower().startswith("mailto:")
+    if url.lower().endswith(".php") or url.lower().startswith("mailto:"):
+        return 0
+    return 1
 
 
 def check_legitimate_website(url: str) -> int:
@@ -199,7 +205,7 @@ def check_legitimate_website(url: str) -> int:
     try:
         whois.whois(domain)
         return 1
-    except Exception:
+    except:
         return 0
 
 
@@ -209,5 +215,160 @@ def check_redirects_legitimacy(url: str) -> int:
         redirects_count = len(response.history)
 
         return 1 if redirects_count <= 4 else 0
-    except Exception:
+    except:
+        return 0
+
+
+def check_fake_url_status_bar(url):
+    try:
+        response = requests.get(url)
+        source_code = response.text
+
+        mouse_over_events = re.findall(
+            r'onMouseOver\s*=\s*["\'](.*?)["\']', source_code
+        )
+
+        for event in mouse_over_events:
+            if "window.status" in event:
+                return 1
+
+        return 0
+    except:
+        return 0
+
+
+def check_disable_right_click(url):
+    try:
+        response = requests.get(url)
+        source_code = response.text
+
+        right_click_events = re.findall(r"event\.button\s*==\s*2", source_code)
+
+        if right_click_events:
+            return 1
+        else:
+            return 0
+
+    except:
+        return 0
+
+
+def popup_window(url: str) -> int:
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        pop_ups = soup.find_all("div", class_="popup")
+
+        for pop_up in pop_ups:
+            input_fields = pop_up.find_all("input")
+            for field in input_fields:
+                if field.get("name") == "name" or field.get("name") == "email":
+                    return 0
+
+        return 1
+
+    except Exception as e:
+        print("Error occurred while fetching webpage source code:", e)
+        return 0
+
+
+def check_invisible_iframes(url: str) -> int:
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        iframes = soup.find_all("iframe")
+
+        for iframe in iframes:
+            frame_border = iframe.get("frameborder")
+            if frame_border is not None and frame_border == "0":
+                return 1
+
+        return 0
+
+    except:
+        return 0
+
+
+def check_domain_legitimacy(url: str) -> int:
+    try:
+        domain_info = whois.whois(url)
+
+        creation_date = domain_info.creation_date
+
+        if isinstance(creation_date, list):
+            creation_date = creation_date[0]
+
+        today = datetime.now()
+        age = today - creation_date
+
+        age_months = age.days / 30
+
+        min_age_legitimate = 6
+
+        if age_months >= min_age_legitimate:
+            return 1
+        else:
+            return 0
+
+    except:
+        return 0
+
+
+def check_dns_and_whois(domain: str) -> int:
+    try:
+        domain_info = whois.whois(domain)
+
+        if not domain_info:
+            return 0
+
+        if not domain_info.name_servers:
+            return 0
+
+        return 1
+
+    except:
+        return 0
+
+
+def check_alexa_rank(domain):
+    try:
+        api = alexapy.API()
+
+        rank_info = api.rank(domain)
+
+        if not rank_info:
+            return 0
+
+        if rank_info["rank"] is None or rank_info["rank"] > 100000:
+            return 0
+
+        return 1
+
+    except:
+        return 0
+
+
+def get_page_rank(url):
+    try:
+        search_results = search("info:" + url, num=1, stop=1, pause=2)
+
+        for result in search_results:
+            if result.startswith("http"):
+                return 1
+        return 0
+    except:
+        return 0
+
+
+def check_google_index(url):
+    try:
+        search_results = search("info:" + url, num=1, stop=1, pause=2)
+
+        for result in search_results:
+            if result.startswith("http"):
+                return 1
+        return 0
+    except:
         return 0
