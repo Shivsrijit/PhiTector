39023d4a7170e6be677beb400196a3715ee8f4d2
hotaru <hotaru.py@gmail.com>
2024-04-18 13:00:31 +0530

    pushed to branch

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,8 +7,8 @@
         <p class='text-xl'>Detect potential phishing websites!</p><br />    
         <input class='px-2 py-2 border-black rounded-xl' placeholder="Enter website name" /><br /><br />
         <div class='bg-[#F4F4F4] shadow-md hover:bg-sky-100 w-fit rounded-2xl px-3 py-3'>
-            <button on:click={() => {}"}}>Check</button>
+            <button on:click={() => {}}>Check</button>
         </div>
-        <
+        
     </div>
 </div>
