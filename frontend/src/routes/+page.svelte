57207f84cb82a6caa6d34896fef4c95e07513e95
hotaru <hotaru.py@gmail.com>
2024-04-18 12:54:46 +0530

    some frontend implementation

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class='flex  items-center min-h-screen w-full px-10'>
+    <div class=''>
+        <h1 class='py-50 text-6xl'>Phitector</h1><br />
+        <p class='text-xl'>Detect potential phishing websites!</p><br />    
+        <input class='px-2 py-2 border-black rounded-xl' placeholder="Enter website name" /><br /><br />
+        <div class='bg-[#F4F4F4] shadow-md hover:bg-sky-100 w-fit rounded-2xl px-3 py-3'>
+            <button on:click={() => {}"}}>Check</button>
+        </div>
+        <
+    </div>
+</div>
