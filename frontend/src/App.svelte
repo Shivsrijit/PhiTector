fac3931ee2bf2cec30f1d11016f6713def8affba
try3d <rsarans186@gmail.com>
2024-04-20 14:07:36 +0530

    yeah there are

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -18,13 +18,18 @@
 
 <input bind:value={url} />
 
-<table>
-  <thead></thead>
-  <tbody></tbody>
-</table>
+
 
 <button on:click={handleClick}>Submit</button>
 
 {#if clicked}
+  <p>Accuracy: {resp.accuracy}</p>
   <p>{JSON.stringify(resp)}</p>
+  <table>
+    <thead>
+      <tr></tr>
+      <td></td>
+    </thead>
+    <tbody></tbody>
+  </table>
 {/if}
